862ec622f61fbfb1b93633e90e1b55592b75fa91
Rick South <rick@natamus.com>
2023-12-12 10:37:44 +0100

    Crying Ghasts source code for 1.20.4.

---
+++ Forge/src/main/java/com/natamus/cryingghasts/ModForge.java
@@ -0,0 +1,36 @@
+package com.natamus.cryingghasts;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.cryingghasts.forge.config.IntegrateForgeConfig;
+import com.natamus.cryingghasts.forge.events.ForgeGhastEvent;
+import com.natamus.cryingghasts.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeGhastEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
