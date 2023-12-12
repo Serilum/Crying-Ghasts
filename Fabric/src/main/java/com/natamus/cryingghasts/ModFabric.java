3c9923737990018dbc71e74fd97c78ef107c2077
Rick South <rick@natamus.com>
2023-12-12 10:37:23 +0100

    Crying Ghasts source code for 1.20.0.

---
+++ Fabric/src/main/java/com/natamus/cryingghasts/ModFabric.java
@@ -0,0 +1,32 @@
+package com.natamus.cryingghasts;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import com.natamus.cryingghasts.events.GhastEvent;
+import com.natamus.cryingghasts.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectivePlayerEvents.PLAYER_TICK.register((ServerLevel world, ServerPlayer player) -> {
+			GhastEvent.onPlayerTick(world, player);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
