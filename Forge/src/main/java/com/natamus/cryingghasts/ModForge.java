6b450cdbabc21e0eeab0c639558829f0eff6acf0
Rick South <rick@natamus.com>
2024-07-28 07:38:40 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/cryingghasts/ModForge.java
+++ Forge/src/main/java/com/natamus/cryingghasts/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.cryingghasts;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.cryingghasts.forge.config.IntegrateForgeConfig;
 import com.natamus.cryingghasts.forge.events.ForgeGhastEvent;
 import com.natamus.cryingghasts.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
--- NeoForge/src/main/java/com/natamus/cryingghasts/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/cryingghasts/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.cryingghasts;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.cryingghasts.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.cryingghasts.neoforge.events.NeoForgeGhastEvent;
 import com.natamus.cryingghasts.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
