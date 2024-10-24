fa76fa97a12635645b4adb624e6243e3c8e15af5
Rick South <rick@natamus.com>
2024-10-24 20:07:40 +0200

    Crying Ghasts source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/cryingghasts/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.cryingghasts;
+
+import com.natamus.cryingghasts.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
