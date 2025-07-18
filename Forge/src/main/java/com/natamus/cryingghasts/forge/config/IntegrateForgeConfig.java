b504fd73d9dae91f817fbe79a93ffdb9d0adcae0
Rick South <rick@natamus.com>
2025-07-18 03:08:07 +0200

    Crying Ghasts source code for 1.21.8.

---
+++ Forge/src/main/java/com/natamus/cryingghasts/forge/config/IntegrateForgeConfig.java
@@ -0,0 +1,16 @@
+package com.natamus.cryingghasts.forge.config;
+
+import com.natamus.collective.config.DuskConfig;
+import com.natamus.cryingghasts.util.Reference;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
+
+public class IntegrateForgeConfig {
+	public static void registerScreen(ModLoadingContext modLoadingContext) {
+		modLoadingContext.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> {
+			return new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> {
+				return DuskConfig.DuskConfigScreen.getScreen(screen, Reference.MOD_ID);
+			});
+		});
+	}
+}
